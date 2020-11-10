2167b410978a4bf93212ad99267f59eb8847d765
00gimsara00 <74104657+00gimsara00@users.noreply.github.com>
2020-11-10 18:25:07 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -24,18 +24,16 @@ PM_START = """Hello {}, my name is {}!
 
 You know how hard it is sometimes to manage group so here is the solution for you
 
-I'm group manager bot and Anti-spam for RR Players🃏
+I'm group manager bot 🃏
 
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Made from the source code of [GIMSARA](t.me/S1_TOURBO)
 
-Special thanks to [this person](t.me/denver02) for helping me.
+Special thanks to [RAVIYA](t.me/MrRaviya) for helping me.
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
 
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
